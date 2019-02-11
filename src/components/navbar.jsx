290294c0308de21185545d5f527c4a97a116e9f1
deathcode <carotkut12@gmail.com>
2019-02-12 00:14:32 +0530

    converted NavBar Component to StatelessFunctionalComponent

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,33 @@
-import React, { Component } from "react";
-class NavBar extends Component {
-  state = {};
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          NavBar
-          <span className="badge badge-pill badge-secondary m-2">
-            {this.props.total}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+import React from "react";
+
+// state less functional component rather then class, as it does not have any state, and eventHandlers
+const NavBar = props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        NavBar
+        <span className="badge badge-pill badge-secondary m-2">
+          {props.total}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+// class NavBar extends Component {
+//   state = {};
+//   render() {
+//     return (
+//       <nav className="navbar navbar-light bg-light">
+//         <a className="navbar-brand" href="#">
+//           NavBar
+//           <span className="badge badge-pill badge-secondary m-2">
+//             {this.props.total}
+//           </span>
+//         </a>
+//       </nav>
+//     );
+//   }
+// }
 
 export default NavBar;
